0dd0371e35727b84559adbe3d2a695d7d8c32f7b
ProlightHub <evan.sweet@example.com>
2020-06-01 22:08:19 -0600

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,52 @@
+var config = {
+    type: Phaser.AUTO,
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 480
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'assets/space3.png');
+    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image('red', 'assets/red.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    var particles = this.add.particles('red');
+
+    var emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    var logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+
+    this.input.mouse.disableContextMenu();
+}
